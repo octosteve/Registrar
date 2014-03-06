bae3ed62c57452e49b82011562af1075339f0c08
Steven Nunez <steven.nunez@gmail.com>
2014-03-06 10:46:33 -0500

    Finish registrar class

--- lib/registrar/registration.rb
+++ lib/registrar/registration.rb
@@ -1,21 +1,33 @@
 module Registrar
+  class InvalidPhoneError < StandardError; end;
   class Registration
     attr_reader :student_name, 
       :student_email, 
       :student_phone, 
       :subject,
       :payment_status,
-      :availability
+      :availability,
+      :messages
 
     def initialize(student)
       @student_name = "#{student[:first_name]} #{student[:last_name]}"
-      @student_email = student[:email]
+      @student_email = student[:email].downcase
       @student_phone = process_phone(student[:phone])
       @subject, @payment_status, @availability = student[:details].split(':')
+      @messages = []
+    end
+
+    def execute!
+      if acceptable?
+        @messages << "Here's a link for your #{@subject} prework. Get on it"
+      end
     end
 
     def process_phone(raw_phone)
       s = raw_phone.scan(/\d/)
+      if s.length != 10
+        raise InvalidPhoneError
+      end
       "(#{s[0]+s[1]+s[2]}) #{s[3]+s[4]+s[5]}-#{s[6]+s[7]+s[8]+s[9]}"
     end
 
--- spec/registrar/registration_spec.rb
+++ spec/registrar/registration_spec.rb
@@ -2,15 +2,17 @@ require 'spec_helper'
 
 module Registrar
   describe Registration do
-    it 'registers a user' do
-      student = {
+    let(:student) do 
+      {
         first_name: "Steven",
         last_name: "Nunez",
         email: "steven@flatironschool.com",
         phone: '702.386.5397',
         details: 'ruby:paid:immediately'
       }
+    end
 
+    it 'sets up user information' do
       registration = Registration.new(student)
       expect(registration.acceptable?).to be_true
       expect(registration.student_name).to eq "Steven Nunez"
@@ -21,11 +23,47 @@ module Registrar
       expect(registration.availability).to eq 'immediately'
     end
 
-    it "does not register a student if they haven't paid"
-    it "sends prework notification for their respective class"
-    it "rejects invalid numbers"
-    it "formats phone numbers"
-    it "normalizes email address"
-    it "doesn't know how to deal with poorly formatted details"
+    context "unacceptable registration" do
+      it "does not register a student if they haven't paid" do
+        student[:details] = 'ruby:due:immediately'
+        registration = Registration.new(student)
+        expect(registration.acceptable?).to be_false
+      end
+
+      it "sends no notification" do
+        student[:details] = 'ruby:due:immediately'
+        registration = Registration.new(student)
+        registration.execute!
+        expect(registration.messages.count).to eq 0
+      end
+    end
+
+    context "acceptable registration" do
+      it "sends prework notification for their respective class" do
+        registration = Registration.new(student)
+        registration.execute!
+        expect(registration.messages.count).to eq 1
+      end
+    end
+
+    it "rejects invalid numbers" do 
+      student[:phone] =  '123'
+      expect {Registration.new(student)}.to raise_error(Registrar::InvalidPhoneError)
+
+      student[:phone] =  'invalid'
+      expect {Registration.new(student)}.to raise_error(Registrar::InvalidPhoneError)
+    end
+
+    it "formats phone numbers" do
+      student[:phone] =  '7023-86-5397'
+      registration = Registration.new(student)
+      expect(registration.student_phone).to eq "(702) 386-5397"
+    end
+
+    it "normalizes email address" do 
+      student[:email] =  "STEVEN@FlaTironsCHool.com"
+      registration = Registration.new(student)
+      expect(registration.student_email).to eq "steven@flatironschool.com"
+    end
   end
 end
